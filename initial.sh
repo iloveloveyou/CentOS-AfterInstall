f2fb0c3911a10536df3788fa412116fb3b2f34df
agno01 <andrej.golis@gmail.com>
2013-05-24 11:21:44 +0200

    sysctl typo fix

--- initial.sh
+++ initial.sh
@@ -186,32 +186,32 @@ system_settings() {
 
 	echo -e "Hardening Sysctl network settings"
 	# Packet forwarding
-	sysctl -w net.ipv4.ip_forward = 0
+	sysctl -w net.ipv4.ip_forward=0
 	# ICMP redirects
-	sysctl -w net.ipv4.conf.all.send_redirects = 0
-	sysctl -w net.ipv4.conf.default.send_redirects = 0
-	sysctl -w net.ipv4.conf.all.accept_redirects = 0
-	sysctl -w net.ipv4.conf.all.secure_redirects = 0
-	sysctl -w net.ipv4.conf.default.accept_redirects = 0
-	sysctl -w net.ipv4.conf.default.secure_redirects = 0
+	sysctl -w net.ipv4.conf.all.send_redirects=0
+	sysctl -w net.ipv4.conf.default.send_redirects=0
+	sysctl -w net.ipv4.conf.all.accept_redirects=0
+	sysctl -w net.ipv4.conf.all.secure_redirects=0
+	sysctl -w net.ipv4.conf.default.accept_redirects=0
+	sysctl -w net.ipv4.conf.default.secure_redirects=0
 	# SYN backlog size - double the size
-	sysctl -w net.ipv4.tcp_max_syn_backlog = 2048
+	sysctl -w net.ipv4.tcp_max_syn_backlog=2048
 	# PING broadcasts
-	sysctl -w net.ipv4.icmp_echo_ignore_broadcasts = 1
+	sysctl -w net.ipv4.icmp_echo_ignore_broadcasts=1
 	# Source routing ability
-	sysctl -w net.ipv4.conf.all.accept_source_route = 0
-	sysctl -w net.ipv4.conf.default.accept_source_route = 0
+	sysctl -w net.ipv4.conf.all.accept_source_route=0
+	sysctl -w net.ipv4.conf.default.accept_source_route=0
 	# Log Martian packets
-	sysctl -w net.ipv4.conf.all.log_martians = 1
+	sysctl -w net.ipv4.conf.all.log_martians=1
 	# Ignore bogus ICMP error responses
-	sysctl -w net.ipv4.icmp_ignore_bogus_error_responses = 1
+	sysctl -w net.ipv4.icmp_ignore_bogus_error_responses=1
 	# SYN flood attacks protection
-	sysctl -w net.ipv4.tcp_syncookies = 1
+	sysctl -w net.ipv4.tcp_syncookies=1
 	# Drop packets from wrong interface (will be logged as Martian)
-	sysctl -w net.ipv4.conf.all.rp_filter = 1
-	sysctl -w net.ipv4.conf.default.rp_filter = 1
+	sysctl -w net.ipv4.conf.all.rp_filter=1
+	sysctl -w net.ipv4.conf.default.rp_filter=1
 	# No timestamps
-	sysctl -w net.ipv4.tcp_timestamps = 0
+	sysctl -w net.ipv4.tcp_timestamps=0
 
 	##########################
 	# TCP wrapper - SSH only #
